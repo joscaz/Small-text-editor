72df6edeb4fa7fac301a1b984e4bd6a2a1f66690
jzertuch <joscazertuche@gmail.com>
2024-03-02 13:01:33 -0600

    Append buffer 2.0

--- kilo.c
+++ kilo.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <string.h>
 
 
 /*** defines ***/
@@ -118,30 +119,62 @@ int getWindowSize(int *rows, int *cols){
     }
 }
 
+
+/*** append buffer ***/
+
+// C does not have dynamic strings, so we'll create ours that supports append operation
+struct abuf{
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+// Append operation
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+// Destructor
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
+
+
 /*** output ***/
 
 //Handles drawing rows of the buffer of text being edited.
-void editorDrawRows(){
+void editorDrawRows(struct abuf *ab){
     int y;
     for (y = 0; y < E.screenrows; y++){
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
 
         if(y < E.screenrows - 1){
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen(){
-    write(STDOUT_FILENO, "\x1b[2J", 4); // 4 means we're writing 4 bytes to terminal
+    struct abuf ab = ABUF_INIT;
+
+    abAppend(&ab, "\x1b[2J", 4); // 4 means we're writing 4 bytes to terminal
     //First byte \x1b (escape char) or 27 in dec
     //<esc>[2J command left cursor at bottom of screen
 
-    write(STDOUT_FILENO, "\x1b[H", 3); // H command to position cursor
+    abAppend(&ab, "\x1b[H", 3); // H command to position cursor
 
-    editorDrawRows();
+    editorDrawRows(&ab);
 
-    write(STDOUT_FILENO, "\x1b[H", 3); // Escape sequence to reposition cursor back up to top-left corner
+    abAppend(&ab, "\x1b[H", 3); // Escape sequence to reposition cursor back up to top-left corner
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 
