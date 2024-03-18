ded78c62e00816ebcf2718e10877ea50adbcf9b1
joscaz <joscazertuche@gmail.com>
2024-03-18 13:23:43 -0600

    Press enter key

--- kilo.c
+++ kilo.c
@@ -243,10 +243,12 @@ void editorUpdateRow(erow *row) {
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len){
-    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len){
+    if (at < 0 || at > E.numrows) return;
 
-    int at = E.numrows;
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -305,10 +307,25 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
     if (E.cy == E.numrows) {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorInsertNewLine() {
+    if (E.cx == 0) {
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDelChar() {
@@ -362,7 +379,7 @@ void editorOpen(char *filename){
     ssize_t linelen;
     while ((linelen = getline(&line, &linecap, fp) != 1)) {
         while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows ,line, linelen);
     }
     free(line);
     fclose(fp);
@@ -571,14 +588,14 @@ void editorMoveCursor(int key){
 }
 
 // Waits for a keypress and handles it.
-0void editorProcessKeypress(){
+void editorProcessKeypress(){
     static int quit_times = KILO_QUIT_TIMES;
 
     int c = editorReadKey();
 
     switch (c){
         case '\r':
-            /* TODO */
+            editorInsertNewLine();
             break;
 
         case CTRL_KEY('q'):
