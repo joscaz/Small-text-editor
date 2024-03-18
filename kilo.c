1829c3799cc6e4ff24c8bce4bdf38360276ab23f
joscaz <joscazertuche@gmail.com>
2024-03-18 17:24:24 -0600

    Detect filetype

--- kilo.c
+++ kilo.c
@@ -17,7 +17,6 @@
 #include <stdarg.h>
 #include <fcntl.h>
 
-
 /*** defines ***/
 
 #define KILO_VERSION "0.0.1"
@@ -45,8 +44,15 @@ enum editorHighlight {
     HL_MATCH
 };
 
+#define HL_HIGHLIGHT_NUMBERS (1<<0)
 
 /*** data ***/
+
+struct editorSyntax {
+    char *filetype;
+    char **filematch;
+    int flags;
+};
 
 typedef struct erow {
     int size;
@@ -69,11 +75,25 @@ struct editorConfig{
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
+    struct editorSyntax *syntax;
     struct termios orig_termios;
 };
 
 struct editorConfig E;
 
+/*** filetypes ***/
+char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
+
+struct editorSyntax HLDB[] = {
+        {
+            "c",
+            C_HL_extensions,
+            HL_HIGHLIGHT_NUMBERS
+        },
+};
+
+// HLDB = Highlight database
+#define HLDB_ENTRIES (sizeof(HDLB) / sizeof(HLDB[0])) // Constant to store length of HLDB array
 
 /*** prototypes ***/
 void editorSetStatusMessage(const char *fmt, ...);
@@ -213,7 +233,6 @@ int getWindowSize(int *rows, int *cols){
     }
 }
 
-
 /*** syntax highlighting ***/
 
 int is_separator(int c) {
@@ -224,6 +243,8 @@ void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
+    if (E.syntax == NULL) return;
+
     int prev_sep = 1;
 
     int i = 0;
@@ -231,11 +252,13 @@ void editorUpdateSyntax(erow *row) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-        if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)){
-            row->hl[i] = HL_NUMBER;
-            i++;
-            prev_sep = 0;
-            continue;
+        if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
+            if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
+                row->hl[i] = HL_NUMBER;
+                i++;
+                prev_sep = 0;
+                continue;
+            }
         }
         prev_sep = is_separator(c);
         i++;
@@ -250,6 +273,32 @@ int editorSyntaxToColor(int hl) {
     }
 }
 
+void editorSelectSyntaxHighlight() {
+    E.syntax = NULL;
+    if (E.filename == NULL) return;
+
+    char *ext = strrchr(E.filename, '.');
+
+    for (unsigned int j = 0; j < HLDB_ENTRIES; j++) {
+        struct editorSyntax *s = &HLDB[j];
+        unsigned int i = 0;
+        while (s->filematch[i]) {
+            int is_ext = (s->filematch[i][0] == '.');
+            if ((is_ext && ext && !strcmp(ext, s->filematch[i])) ||
+                    (!is_ext && strstr(E.filename, s->filematch[i]))) {
+                E.syntax = s;
+
+                int filerow;
+                for (filerow = 0; filerow < E.numrows; filerow++) {
+                    editorUpdateSyntax(&E.row[filerow]);
+                }
+
+                return;
+            }
+            i++;
+        }
+    }
+}
 
 /*** row operations ***/
 
@@ -363,7 +412,6 @@ void editorRowDelChar(erow *row, int at) {
     E.dirty++;
 }
 
-
 /*** editor operations ***/
 
 void editorInsertChar(int c) {
@@ -405,7 +453,6 @@ void editorDelChar() {
     }
 }
 
-
 /*** file i/o  ***/
 
 char *editorRowsToString(int *buflen) {
@@ -432,6 +479,8 @@ void editorOpen(char *filename){
     free(E.filename);
     E.filename = strdup(filename);
 
+    editorSelectSyntaxHighlight();
+
     FILE *fp = fopen(filename, "r");
     if (!fp) die("fopen");
 
@@ -454,6 +503,7 @@ void editorSave() {
             editorSetStatusMessage("Save aborted");
             return;
         }
+        editorSelectSyntaxHighlight();
     }
 
     int len;
@@ -475,7 +525,6 @@ void editorSave() {
     free(buf);
     editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
-
 
 /*** find  ***/
 
@@ -548,7 +597,6 @@ void editorFind() {
     }
 }
 
-
 /*** append buffer ***/
 
 // C does not have dynamic strings, so we'll create ours that supports append operation
@@ -573,7 +621,6 @@ void abAppend(struct abuf *ab, const char *s, int len){
 void abFree(struct abuf *ab){
     free(ab->b);
 }
-
 
 /*** output ***/
 
@@ -658,8 +705,8 @@ void editorDrawStatusBar(struct abuf *ab) {
     int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
                        E.filename ? E.filename : "[No Name]", E.numrows,
                        E.dirty ? "(modified)" : "");
-    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
-                        E.cy + 1, E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d",
+                        E.syntax ? E.syntax->filetype : "no ft", E.cy + 1, E.numrows);
     if (len > E.screencols) len = E.screencols;
     abAppend(ab, status, len);
     while (len < E.screencols) {
@@ -882,6 +929,7 @@ void initEditor(){
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
+    E.syntax = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
     E.screenrows -= 2;
